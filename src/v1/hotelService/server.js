7528fe3172d1d330678adcbf180ca0c8c5c487f5
jaysojitra13 <jaysojitra13@gmail.com>
2021-05-31 23:32:14 +0530

    Add clustering in modules

--- src/v1/hotelService/server.js
+++ src/v1/hotelService/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const cluster = require('cluster');
 const cors = require('cors');
 
 dotenv.config({ path: '../../../.env' });
@@ -14,6 +15,15 @@ app.use(cors());
 
 app.use('/', require('./routes/index'));
 
-app.listen(process.env.HOTEL_SERVICE_PORT, () => {
-  console.log(`Hotel Service is listening on port ${process.env.HOTEL_SERVICE_PORT}`);
-});
+if (cluster.isMaster) {
+  // If traffic to this pod will increase in future then we can add more .fork(). Max number
+  // of fork should be equeal to number of cores of cpu
+  console.log(`Master ${process.pid} is running`);
+  cluster.fork();
+  cluster.fork();
+} else {
+  app.listen(process.env.HOTEL_SERVICE_PORT, () => {
+    console.log(`Hotel Service is listening on port ${process.env.HOTEL_SERVICE_PORT}`);
+    console.log(`Worker ${process.pid} started`);
+  });
+}
